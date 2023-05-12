0145cdca3883db604558eafb8a12b9897a8fc88a
Alina Shigera <129881198+a11ver@users.noreply.github.com>
2023-05-13 00:43:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_2
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = Console.ReadLine();
+            student.Id = Convert.ToInt32(Console.ReadLine());
+            student.MyValidation();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace laba_2
+{
+    public class Student : IValidateble
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+        [Range(1111, 3333)]
+        public int Id { get; set; }
+    }
+}
